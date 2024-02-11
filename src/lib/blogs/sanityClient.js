7415615d650c41da52bf0701763e068c0453dab6
Annika <137914274+arguemore@users.noreply.github.com>
2024-02-12 01:26:14 +0800

    Update sanityClient.js

--- src/lib/blogs/sanityClient.js
+++ src/lib/blogs/sanityClient.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 const client = sanityClient({
-	projectId: 'u62jjaxi',
+	projectId: 'a7fk82qa',
 	dataset: 'production',
 	apiVersion: '2023-01-05',
 	useCdn: false // development
